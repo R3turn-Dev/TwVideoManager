618ac2ed50b03d74c1a43a43dd0207e6da9f5a47
EunhakLee <admin@return0927.xyz>
2019-09-24 21:36:13 +0900

    Added example configuration file

---
+++ config.py
@@ -0,0 +1,14 @@
+"""
+Example configuration file
+"""
+
+###################
+#     Twitch      #
+###################
+
+# Twitch OAuth2 Access Token
+# You can get one from Twitch login redirection or
+# just copying from initial socket communication in Twitch chatroom.
+_TWITCH_CLIENT_ID = ""
+_TWITCH_OAUTH_TOKEN = ""
+
