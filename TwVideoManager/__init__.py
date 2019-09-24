e3f45ea5cbad1241d3fa598ad41ff513da36038d
EunhakLee <admin@return0927.xyz>
2019-09-24 19:47:21 +0900

    Add models into TwVideoManager's subclass

--- TwVideoManager/__init__.py
+++ TwVideoManager/__init__.py
@@ -10,3 +10,8 @@ __VERSION__ = "0.0.1b"
 __DATE__ = "20190924"
 
 from . import gdrive, misc, twitch
+
+from .models import Video
+from .gdrive import RemoteVideoFile
+from .twitch import TwitchVideo
+from .misc import VideoFile
