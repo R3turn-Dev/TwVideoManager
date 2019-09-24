c48da9eb8198a0a372ffb674a9c63999c76a770c
EunhakLee <admin@return0927.xyz>
2019-09-24 21:35:57 +0900

    Added new Twitch API dependency

--- setup.py
+++ setup.py
@@ -9,15 +9,32 @@ with open("TwVideoManager/__init__.py") as f:
     version = re.search(r'^__VERSION__\s=\s[\'"]([^\'"]*)[\'"]', data, re.MULTILINE).group(1)
 
     if version.endswith(("a", "b", "rp")):
-        version += re.search(r'^__DATE__\s=\s[\'"](\d+)[\'"]', data, re.MULTILINE).group(1)
+        version += "-" + re.search(r'^__DATE__\s=\s[\'"](\d+)[\'"]', data, re.MULTILINE).group(1)
+
+# Reading long description
+long_description = open("README.md", encoding="UTF-8").read()
+
+# Reading requirements
+requirements = ''
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
 
 setup(
     name='TwVideoManager',
     version=version,
     packages=['TwVideoManager'],
+    requirements=requirements,
     url='https://github.com/R3turn-Dev/TwVideoManager/',
     license='LGPL-3.0',
     author='Eunhak Lee(return0927)',
     author_email='admin@return0927.xyz',
     description='Unified Twitch Video manager via Local and Google Drive',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+    ],
     )
