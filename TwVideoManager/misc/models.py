dadd763e1e3ec79f3e145dee493307c6b398c87d
EunhakLee <admin@return0927.xyz>
2019-09-26 16:22:08 +0900

    [Misc] Made VideoFile be a mixin between File and Video
    VideoFile must have properties all the both of File and Video, so to be
    managed as a whole strucrue at once.
    
    - Open-mode 'rb' may return `BufferedReader` but firstly implemented as `TextIOWrapper`
    - Made `VideoFile` into a mixin between File and Video.

--- TwVideoManager/misc/models.py
+++ TwVideoManager/misc/models.py
@@ -1,9 +1,9 @@
-from _io import TextIOWrapper
+from _io import BufferedReader
 from dataclasses import dataclass
 # For variable static typing
 from datetime import datetime
 from os import path
-from typing import Any, List, Optional
+from typing import Any, List, Optional, Union
 
 from ..models import Video
 
@@ -60,7 +60,7 @@ class Directory:
         return out
 
 @dataclass()
-class VideoFile(Video):
+class VideoFile(Video, File):
     """Video files in local data directory
 
     :arg str name: The name of the video file
@@ -72,26 +72,39 @@ class VideoFile(Video):
 
     name: str
     extension: str
-    file: TextIOWrapper
+    file: BufferedReader
+
 
     def __repr__(self):
         return f"""<VideoFile `{self.name}` of `{self.author}`>"""
 
     @staticmethod
-    def from_file(file: TextIOWrapper):  # TODO: Doc this func
-        if file.mode != "rb":
-            raise TypeError("Video file must be opened with binary mode.")
+    def from_file(file: Union[BufferedReader, File], author=None, date=None):  # TODO: Doc this func
+        if isinstance(file, BufferedReader):
+            if file.mode != "rb":
+                raise TypeError("Video file must be opened with binary mode.")
 
-        full_path = path.abspath(file.name).replace("\\", "/")
-        name: str= full_path.split("/")[-1]
+            full_path = path.abspath(file.name).replace("\\", "/")
+            name: str= full_path.split("/")[-1]
 
-        if "." not in name:
-            extension = None
+            if "." not in name:
+                extension = None
+            else:
+                extension, name = name[::-1].split(".", 1)
+                name, extension = name[::-1], extension[::-1]
+
+            vid_author: str = path.dirname(file.name).replace("\\", "/").split("/")[-1]
+            vid_date: datetime = VideoFile.parse_time(name)
+
+            return VideoFile(name=name, extension=extension, file=file, author=author or vid_author, date=date or vid_date)
+        elif isinstance(file, File):
+            if author == None or date == None:
+                raise TypeError('author and date must be provided if the file is an instance of `File` model.')
+
+            name = file.name
+            extension = file.extension
+            file: BufferedReader = open(path.join(file.path, file.name), 'rb')
+
+            return VideoFile(author, date, name, extension, file)
         else:
-            extension, name = name[::-1].split(".", 1)
-            name, extension = name[::-1], extension[::-1]
-
-        author: str = path.dirname(file.name).replace("\\", "/").split("/")[-1]
-        date: datetime = VideoFile.parse_time(name)
-
-        return VideoFile(name=name, extension=extension, file=file, author=author, date=date)
+            raise TypeError('File must be subcalss of `File` or `TextIOWrapper.')
