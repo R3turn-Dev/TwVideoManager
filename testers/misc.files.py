3efb976dade83f964b1414c9aa08f563c9064382
EunhakLee <admin@return0927.xyz>
2019-09-30 19:24:21 +0900

    [Testers] Updated the tester `misc.files`
    - Load and Parse files from local Stream directory

--- testers/misc.files.py
+++ testers/misc.files.py
@@ -1,2 +1,8 @@
 from TwVideoManager.misc.files import LocalFinder
+from TwVideoManager.misc.parser import MP4Parser
+
 lf = LocalFinder()
+parser = MP4Parser()
+
+for file in lf.video_files:
+    print(parser.parse(file))
